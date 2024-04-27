ed65170a19376ff532f272227e73246d086c5c33
RasagnaCoder <rasagnauyyala@gmail.com>
2024-04-27 15:30:56 +0530

    commited

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ print("hello")
 print("added hello file in my first repo")
 print("i made changes in git hub")
 print("changes in local")
+<<<<<<< Updated upstream
 print("tpo lab2")
 print("vs code")
+=======
+print("tpo lab2")
+>>>>>>> Stashed changes
